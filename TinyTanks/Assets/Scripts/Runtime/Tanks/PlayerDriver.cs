3fb83252498389357c738fd610522317ed39ef1a
Bobby <bhoogenboom23@gmail.com>
2025-05-14 01:01:12 +0200

    tank movement startup

--- TinyTanks/Assets/Scripts/Runtime/Tanks/PlayerDriver.cs
+++ TinyTanks/Assets/Scripts/Runtime/Tanks/PlayerDriver.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerDriver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    private Rigidbody rb;
+
+    [Header("Variables")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotationSpeed = 100f;
+
+
+
+    [Header("Input Values")]
+    private Vector2 _moveVector; // left track = W/S && right track = ^/v
+
+
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        _moveVector = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.AddForce(Physics.gravity, ForceMode.Acceleration);
+
+        float moveAmount = (_moveVector.y + _moveVector.x) * 0.5f * speed;
+        Vector3 move = transform.forward * moveAmount * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
+
+        float rotationAmount = (_moveVector.y - _moveVector.x) * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnOffset = Quaternion.Euler(0, rotationAmount, 0);
+        rb.MoveRotation(rb.rotation * turnOffset);
     }
 }
