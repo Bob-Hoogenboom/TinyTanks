09411eac6414654469adfad890df031315750726
Tom Jongman <143596035+killerwolf162@users.noreply.github.com>
2025-06-10 15:19:30 +0200

    - added explosion vfx when bullet hits tank

--- TinyTanks/Assets/Scripts/Runtime/Tanks/Bullet.cs
+++ TinyTanks/Assets/Scripts/Runtime/Tanks/Bullet.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject parent;
+    public GameObject explosionVFX;
 
     private void OnTriggerEnter(Collider other)
     {
+        
+
         if (other.gameObject.layer == 3)
         {
             if(other.gameObject != parent)
@@ -17,10 +20,17 @@ public class Bullet : MonoBehaviour
                 else if (other.gameObject.GetComponentInParent<Health>())
                     other.gameObject.GetComponentInParent<Health>().TakeDamage(1);
                 Debug.Log("hit a player");
+                var vxf = Instantiate(explosionVFX, this.transform.position, this.transform.rotation);
+                Destroy(vxf, 3);
                 Destroy(gameObject);
             }         
         }
         else if(other.gameObject.layer != 3)
-            Destroy(gameObject);
+        {
+            var vxf = Instantiate(explosionVFX, this.transform.position, this.transform.rotation);
+            Destroy(vxf, 3);
+            Destroy(gameObject);      
+        }
+
     }
 }
