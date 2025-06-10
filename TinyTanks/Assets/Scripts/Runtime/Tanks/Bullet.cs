cac45a25fc4b58da003fcecfd252da1fc1e411d1
Tom Jongman <143596035+killerwolf162@users.noreply.github.com>
2025-06-10 15:54:36 +0200

    - added heat distortion to tank - added bullet trail vfx to bullet -> needs to be checked by mats

--- TinyTanks/Assets/Scripts/Runtime/Tanks/Bullet.cs
+++ TinyTanks/Assets/Scripts/Runtime/Tanks/Bullet.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject parent;
-    public GameObject explosionVFX;
+
+    [SerializeField] private GameObject _tankHitVFX;
+    [SerializeField] private GameObject _enviormentHitVFX;
+    [SerializeField] private GameObject _bulletTrailVFX;
+
+    private GameObject _bulletTrail;
+
+    private void Start()
+    {
+        _bulletTrail = Instantiate(_bulletTrailVFX, this.transform.position, this.transform.rotation, this.gameObject.transform);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +23,7 @@ public class Bullet : MonoBehaviour
 
         if (other.gameObject.layer == 3)
         {
+            Debug.Log(other.gameObject.name);
             if(other.gameObject != parent)
             {
                 if (other.gameObject.GetComponent<Health>())
@@ -20,15 +31,17 @@ public class Bullet : MonoBehaviour
                 else if (other.gameObject.GetComponentInParent<Health>())
                     other.gameObject.GetComponentInParent<Health>().TakeDamage(1);
                 Debug.Log("hit a player");
-                var vxf = Instantiate(explosionVFX, this.transform.position, this.transform.rotation);
+                var vxf = Instantiate(_tankHitVFX, this.transform.position, this.transform.rotation);
                 Destroy(vxf, 3);
+                Destroy(_bulletTrail, 1);
                 Destroy(gameObject);
             }         
         }
-        else if(other.gameObject.layer != 3)
+        else if(other.gameObject.layer != 3 && other.gameObject.layer != 7)
         {
-            var vxf = Instantiate(explosionVFX, this.transform.position, this.transform.rotation);
+            var vxf = Instantiate(_enviormentHitVFX, this.transform.position, this.transform.rotation);
             Destroy(vxf, 3);
+            Destroy(_bulletTrail, 1);
             Destroy(gameObject);      
         }
 
