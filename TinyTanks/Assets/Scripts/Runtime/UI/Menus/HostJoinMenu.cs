ad0a214c1a73fa8fe564a76a6c58a43cd17c1115
Bobby <bhoogenboom23@gmail.com>
2025-09-11 15:29:38 +0200

    NetworkLoginPage

--- TinyTanks/Assets/Scripts/Runtime/UI/Menus/HostJoinMenu.cs
+++ TinyTanks/Assets/Scripts/Runtime/UI/Menus/HostJoinMenu.cs
@@ -1,27 +1,49 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Menu
 {
     public class HostJoinMenu : MonoBehaviour
     {
         private NetworkManager _netManager;
-        public InputField ipInput;
+        public TMP_InputField ipInput;
+        public TMP_InputField portInput;
 
         private void Awake()
         {
-            _netManager = GetComponent<NetworkManager>();
+            _netManager = FindObjectOfType<NetworkManager>();
         }
 
         public void HostButton()
         {
+            //#TODO does the host need to fill in their own IP and port? or should this be done automatic?
             _netManager.StartHost();
         }
 
         public void JoinButton()
         {
+            UpdateNetwork();
+            _netManager.StartClient();
+        }
 
+        private void UpdateNetwork()
+        {
+            // set the IP
+            _netManager.networkAddress = ipInput.text;
+
+            // set the port if transport supports it
+            if (Transport.active is PortTransport portTransport)
+            {
+                if (ushort.TryParse(portInput.text, out ushort port))
+                {
+                    portTransport.Port = port;
+                }
+                else
+                {
+                    Debug.LogWarning("Invalid port entered. Using default.");
+                }
+            }
         }
     }
 }
