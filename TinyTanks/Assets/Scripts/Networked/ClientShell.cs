53b6d0e4c3616d1bcf629e0e5a2a91602ed3cbf1
Tom Jongman <143596035+killerwolf162@users.noreply.github.com>
2025-09-25 10:21:02 +0200

    - remove unneeded scripts

--- TinyTanks/Assets/Scripts/Networked/ClientShell.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Mirror;
-
-public class ClientShell : MonoBehaviour
-{
-    public NetworkedShell shellParent;
-}
